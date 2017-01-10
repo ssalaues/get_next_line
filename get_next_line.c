fa95a6a86c53f11fe761d94ed6ce9486b41d48ad
Salim SALAUES <ssalaues@e1z2r9p42.42.us.org>
2017-01-09 17:27:38 -0800

    Converted to lists

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,20 @@
 /*   By: ssalaues <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/23 16:32:08 by ssalaues          #+#    #+#             */
-/*   Updated: 2017/01/06 22:41:44 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/01/09 16:54:26 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-static  char *ft_restr(char *old, char *new)
-{
-	char *rt;
 
-	rt = ft_strnew(ft_strlen(old) + BUFF_SIZE + 1);
-	rt = ft_strncat(old, new, ft_wordlen(new, '\n') + ft_strlen(old));
-	if (*old)
-		free(old);
-	return(rt);
-}
-//static char *ft_strndup(const char *s1, size_t n)
-//{
-//}
-
+/*
 int	get_next_line(const int fd, char **line)
 {
 	char			t1[BUFF_SIZE + 1];
 	char			*t2;
 	size_t			ct;
 	size_t			ct2 = 0;
-	static t_list	*rdin;
+//	static t_list	*rdin;
 
 	ct = 0;
 	bzero(t1, BUFF_SIZE + 1);
@@ -48,4 +36,39 @@ int	get_next_line(const int fd, char **line)
 		}
 	}
 	return (0); //Reading Complete EOF
+}*/
+// With Lists
+static char	*ft_strndup(const char *s1, size_t n)
+{
+	char *dup;
+
+	dup = (char *)malloc(sizeof(char) * (n + 1));
+	if (!dup)
+		return (NULL);
+	else
+		ft_strncpy(dup, s1, n);
+	return (dup);
+}
+int	get_next_line(const int fd, char **line)
+{
+	char			t1[BUFF_SIZE + 1];
+	size_t			bs = 0;
+	int				ct = 0;
+	static t_list	*rdin;
+
+	ft_bzero(t1, BUFF_SIZE + 1);
+	while((bs = read(fd, t1, BUFF_SIZE)))
+	{
+		if (ct == 0)
+			rdin = ft_lstnew((char*)t1, bs + 1);
+//		rdin->content = ft_strncat(rdin->content, t1, ft_wordlen(t1, '\n'));
+		if (ft_strchr(t1, '\n'))
+		{
+			*line = ft_strndup(rdin->content, ft_wordlen(rdin->content, '\n'));
+			free(rdin->content);
+			return (1);
+		}
+		ct++;
+	}
+	return (0);
 }
