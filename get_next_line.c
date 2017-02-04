2e2374d6edcdd5f976c86ab50ed53f8ca23106ee
Salim SALAUES <ssalaues@e1z3r7p6.42.us.org>
2017-02-03 18:53:04 -0800

    Error handling

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ssalaues <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/23 16:32:08 by ssalaues          #+#    #+#             */
-/*   Updated: 2017/01/25 20:35:50 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/02/03 18:52:38 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,16 @@ int	get_next_line(const int fd, char **line)
 	static t_gnl	*bl;
 
 	ft_bzero(t1, BUFF_SIZE + 1);
+	if (fd < 0)
+		return (-1);
 	if (!bl)
 		bl = gnl_lstnew("", BUFF_SIZE + 1, fd);
 	while((bl->bs = read(fd, t1, BUFF_SIZE)) || bl->data != '\0')
 	{
-		if (bl->bs < 0)
-			return (-1);
 		bl->t2 = ft_strjoin(bl->data, t1);
 		ft_bzero(t1, BUFF_SIZE + 1);
 		bl->data = bl->t2;
-		if (ft_strchr(bl->t2, '\n'))
+		if (ft_strchr(bl->t2, '\n') || bl->bs)
 		{
 			*line = ft_strndup(bl->data, ft_wordlen(bl->data, '\n'));
 			bl->data = ft_strchr(bl->data, '\n');
