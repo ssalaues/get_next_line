7edf1a7a83726919a78126d3333e9622b73e01a2
Salim SALAUES <ssalaues@e1z3r5p1.42.us.org>
2017-02-13 20:57:34 -0800

    Leak free

--- get_next_line.c
+++ get_next_line.c
@@ -6,61 +6,77 @@
 /*   By: ssalaues <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/23 16:32:08 by ssalaues          #+#    #+#             */
-/*   Updated: 2017/02/13 14:31:43 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/02/13 20:56:06 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_gnl	*gnl_lstnew(t_gnl *bl, size_t len, int fd)
+t_gnl				*gnl_lstnew(t_gnl *bl, size_t len, int fd)
 {
-	t_gnl	*new;
+	t_gnl			*new;
 
 	if (!bl)
 	{
 		new = (t_gnl *)malloc(sizeof(t_gnl));
-		new->data = (char *)malloc(sizeof(char *));
-		ft_bzero(new->data, 1);
-		new->len = len;
+		new->data = (char *)ft_memalloc(sizeof(char));
+		ft_bzero(new->t1, BUFF_SIZE + 1);
+		new->used = 0;
 		new->fd = fd;
 		new->next = NULL;
 		return (new);
 	}
 	if (bl && bl->fd != fd)
-		bl = gnl_lstnew(bl->next, len, fd);
-	while (bl)
+		bl->next = gnl_lstnew(bl->next, len, fd);
+	new = bl;
+	while (new)
 	{
-		if (bl->fd == fd)
-			return (bl);
-		bl = bl->next;
+		if (new->fd == fd)
+			return (new);
+		new = new->next;
 	}
-	return (NULL);
+	return (bl);
 }
 
-int	get_next_line(const int fd, char **line)
+int				line_cp(t_gnl *bl, char **line)
 {
-	char			t1[BUFF_SIZE + 1];
+		bl->t2 = bl->data;
+		*line = ft_strndup(bl->data, ft_wordlen(bl->data, '\n'));
+		if ((bl->data = ft_strchr(bl->data, '\n')))
+			bl->data++;
+		if (bl->data)
+		{
+			bl->data = ft_strdup(bl->data);
+			ft_strdel(&bl->t2);
+		}
+		return (1);
+}
+
+int					get_next_line(const int fd, char **line)
+{
 	static t_gnl	*bl;
 
-	ft_bzero(t1, BUFF_SIZE + 1);
 	if (!bl || bl->fd != fd)
 		bl = gnl_lstnew(bl, BUFF_SIZE + 1, fd);
-	while ((bl->bs = read(fd, t1, BUFF_SIZE)) || bl->data != 0)
+	while ((bl->bs = read(fd, bl->t1, BUFF_SIZE)) || bl->data != 0)
 	{
-		if (!*t1 && bl->bs < 0)
+		if (!*bl->t1 && bl->bs < 0)
 			return (-1);
-		if (*t1)
-			bl->data = ft_strjoin(bl->data, t1);
-		ft_bzero(t1, BUFF_SIZE + 1);
-		if ((!bl->bs && *bl->data != '\0') || (ft_strchr(bl->data, '\n')))
+		if (*bl->t1)
 		{
-			*line = ft_strndup(bl->data, ft_wordlen(bl->data, '\n'));
-			if ((bl->data = ft_strchr(bl->data, '\n')))
-				bl->data++;
-			return (1);
+			bl->t2 = bl->data;
+			bl->data = ft_strjoin(bl->data, bl->t1);
+			if (bl->t2)
+//				free(bl->t2);
+				ft_strdel(&bl->t2);
 		}
+		ft_bzero(bl->t1, BUFF_SIZE + 1);
+		if ((!bl->bs && *bl->data != '\0') || (ft_strchr(bl->data, '\n')))
+			return (line_cp(bl, line));
 		if (bl->bs == 0)
+		{
 			return (0);
+		}
 	}
 	return (0);
 }
